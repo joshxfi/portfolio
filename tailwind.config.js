37ae522c0810d95c1446559abccdc0c8deac0889
Josh Daniel <joshxfi.dev@gmail.com>
2022-07-05 10:09:52 +0800

    chore: setup assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F9F9F9",
+        secondary: "#0C0E0C",
+      },
+    },
   },
   plugins: [],
-}
+};

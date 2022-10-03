5701a52436f0cb273980ddbed79e5ff00f846307
Josh Daniel <joshxfi.dev@gmail.com>
2022-10-03 11:01:17 +0800

    chore: add roadmap redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  async redirects() {
+    return [
+      {
+        source: "/roadmap",
+        destination:
+          "https://joshxfi.notion.site/Front-end-Roadmap-0b680067078b4884a0628d342058be07",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
